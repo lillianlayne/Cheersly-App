066383bd2c05ba61c4dedcf6314f527611ba683d
lillianlayne <lillianlayne22@gmail.com>
2024-03-29 16:23:02 -0400

    add livereload server for dev run

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+// live reload 
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100)
+})
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+app.use(connectLiveReload());
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +29,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// mount routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+app.get('/', (req, res) => {
+  console.log('user hit the resource');
+  res.send('home')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
